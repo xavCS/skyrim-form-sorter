0af1e7edfad110172260a25a27479e143c27e450
xavCS <80826662+xavCS@users.noreply.github.com>
2021-07-28 15:31:15 +0100

    added comments

--- quitWhile.py
+++ quitWhile.py
@@ -1,8 +1,7 @@
 from pynput import keyboard
-
+#bind a listener to alt gr that breaks from a while loop that depends on x being True
 COMBINATIONS = [
-    {keyboard.Key.alt_gr} #ALT GR
-   
+    {keyboard.Key.alt_gr} #ALT GR  
 ]
 
 current = set()
@@ -12,7 +11,7 @@ def execute():
     global x
     x = False
 
-
+#this probably isn't the most efficient way to go about it, but as of right now i'm not very good with this module
 def on_press(key):
     if any([key in COMBO for COMBO in COMBINATIONS]):
         current.add(key)
@@ -24,4 +23,4 @@ def on_release(key):
         current.remove(key)
 
 listener = keyboard.Listener(on_press=on_press,on_release=on_release)
-listener.start()
+listener.start() #used this instead of context manager because with statement was blocking all other code from running.
